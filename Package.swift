dc45b74ed368e4297b8c1a27de21f9dd3ef1fee1
Kunal More <mkunal065@gmail.com>
2024-05-02 21:06:51 +0530

    package file update for target, framework added

--- Package.swift
+++ Package.swift
@@ -14,10 +14,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MKNetworkManager"),
-        .testTarget(
-            name: "MKNetworkManagerTests",
-            dependencies: ["MKNetworkManager"]),
+        .binaryTarget(
+            name: "MKNetworkManager",
+            path: "./Sources/MKNetworkManager.xcframework")
     ]
 )
---
+++ Sources/MKNetworkManager.xcframework/ios-arm64/MKNetworkManager.framework/Headers/MKNetworkManager.h
@@ -0,0 +1,18 @@
+//
+//  MKNetworkManager.h
+//  MKNetworkManager
+//
+//  Created by Kunal More on 02/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MKNetworkManager.
+FOUNDATION_EXPORT double MKNetworkManagerVersionNumber;
+
+//! Project version string for MKNetworkManager.
+FOUNDATION_EXPORT const unsigned char MKNetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKNetworkManager/PublicHeader.h>
+
+
